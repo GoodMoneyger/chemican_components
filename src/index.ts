17346c27f2401b371bf301d85e584166980da3f1
Kevin Schiffer <mail@kevinschiffer.com>
2025-02-27 14:50:03 +0900

    chore: Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
 import './index.css';
 
 export * from './components';
-export * from './components/Checkbox';
-export * from './components/Chip';
-export * from './components/Dialog';
-export * from './components/Input';
-export * from './components/Progress';
-export * from './components/RadioButton';
-export * from './components/Select';
-export * from './components/Spinner';
-export * from './components/Table';
-export * from './components/Tag';
-export * from './components/Textarea';
-export * from './components/Toast';
