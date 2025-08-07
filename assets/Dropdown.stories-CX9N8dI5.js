5c13eec07692db48d062e07cd60fcd46265d5d1e
kschiffer <mail@kevinschiffer.com>
2025-08-07 01:14:11 +0000

    Deploying to gh-pages from @ GoodMoneyger/chemican_components@7d69a5f499796246f4a0429292221cb642180804 🚀

---
+++ assets/Dropdown.stories-CX9N8dI5.js
@@ -0,0 +1,107 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{B as t}from"./Button-CTemnL9m.js";import{D as e,a as r,b as d,c as o,I as b,d as L,e as m}from"./Dropdown-DwAzX0k-.js";import{I as _}from"./IconUser-Cl9KeqG0.js";import{c as l}from"./createReactComponent-DIAtsXst.js";import"./iframe-Bn-bUIPO.js";import"./preload-helper-Dp1pzeXC.js";import"./index-CRGxjvZy.js";import"./index-DgDDwYtN.js";import"./utils-BCsZDoRo.js";import"./bundle-mjs-DAWI7bSt.js";import"./Spinner-DAFoGoa5.js";import"./index-qoxOkdfh.js";import"./index-bYW-dLQW.js";import"./index-DsCJ4FxG.js";import"./index-BAg2UnU2.js";import"./index-CmnV8HPl.js";import"./index-1uFb-3NZ.js";import"./index-DnqUOlgP.js";import"./index-20yEGz5W.js";import"./index-Dbqht7Rb.js";import"./index-CMs7d4gQ.js";import"./index-CMsyV5NS.js";import"./index-D1ugWK11.js";import"./index-DSGx3Quq.js";import"./index-CZJP2-U1.js";import"./index-BcYDBaBf.js";/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const A=[["path",{d:"M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-0"}],["path",{d:"M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-1"}],["path",{d:"M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-2"}]],N=l("outline","dots","Dots",A);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const z=[["path",{d:"M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1",key:"svg-0"}],["path",{d:"M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z",key:"svg-1"}],["path",{d:"M16 5l3 3",key:"svg-2"}]],W=l("outline","edit","Edit",z);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const E=[["path",{d:"M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2",key:"svg-0"}],["path",{d:"M9 12h12l-3 -3",key:"svg-1"}],["path",{d:"M18 15l3 -3",key:"svg-2"}]],B=l("outline","logout","Logout",E);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const P=[["path",{d:"M4 7l16 0",key:"svg-0"}],["path",{d:"M10 11l0 6",key:"svg-1"}],["path",{d:"M14 11l0 6",key:"svg-2"}],["path",{d:"M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12",key:"svg-3"}],["path",{d:"M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3",key:"svg-4"}]],O=l("outline","trash","Trash",P),wn={title:"Components/Dropdown",component:e,parameters:{layout:"centered"},argTypes:{open:{control:{type:"boolean"},description:"Controls whether the dropdown is open"},modal:{control:{type:"boolean"},description:"Whether the dropdown is modal"}}},U=k=>n.jsxs(e,{...k,children:[n.jsx(r,{asChild:!0,children:n.jsx(t,{intent:"secondary",children:"Open Menu"})}),n.jsxs(d,{children:[n.jsx(o,{icon:_,children:"Profile"}),n.jsx(o,{icon:b,children:"Settings"}),n.jsx(L,{}),n.jsx(o,{icon:B,intent:"danger",children:"Logout"})]})]}),s=U.bind({});s.args={};const i=()=>n.jsxs(e,{children:[n.jsx(r,{asChild:!0,children:n.jsx(t,{intent:"ghost",icon:N})}),n.jsxs(d,{children:[n.jsx(o,{icon:W,children:"Edit"}),n.jsx(o,{icon:O,intent:"danger",children:"Delete"})]})]}),a=()=>n.jsxs(e,{children:[n.jsx(r,{asChild:!0,children:n.jsx(t,{intent:"secondary",children:"Account Menu"})}),n.jsxs(d,{children:[n.jsx(m,{children:"Account"}),n.jsx(o,{icon:_,children:"Profile"}),n.jsx(o,{icon:b,children:"Settings"}),n.jsx(L,{}),n.jsx(m,{children:"Actions"}),n.jsx(o,{icon:B,intent:"danger",children:"Logout"})]})]}),p=()=>n.jsxs("div",{className:"flex gap-4",children:[n.jsxs(e,{children:[n.jsx(r,{asChild:!0,children:n.jsx(t,{intent:"secondary",children:"Default"})}),n.jsxs(d,{children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{children:"Another Item"})]})]}),n.jsxs(e,{children:[n.jsx(r,{asChild:!0,children:n.jsx(t,{intent:"secondary",children:"With Disabled"})}),n.jsxs(d,{children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{disabled:!0,children:"Disabled Item"})]})]})]}),c=()=>n.jsxs("div",{className:"flex gap-4",children:[n.jsxs(e,{children:[n.jsx(r,{asChild:!0,children:n.jsx(t,{intent:"secondary",children:"Small"})}),n.jsxs(d,{size:"sm",children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{children:"Another Item"})]})]}),n.jsxs(e,{children:[n.jsx(r,{asChild:!0,children:n.jsx(t,{intent:"secondary",children:"Medium"})}),n.jsxs(d,{size:"md",children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{children:"Another Item"})]})]}),n.jsxs(e,{children:[n.jsx(r,{asChild:!0,children:n.jsx(t,{intent:"secondary",children:"Large"})}),n.jsxs(d,{size:"lg",children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{children:"Another Item"})]})]})]});i.__docgenInfo={description:"",methods:[],displayName:"WithIcon"};a.__docgenInfo={description:"",methods:[],displayName:"WithLabels"};p.__docgenInfo={description:"",methods:[],displayName:"States"};c.__docgenInfo={description:"",methods:[],displayName:"Sizes"};var h,D,w;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`args => <Dropdown {...args}>
+    <DropdownTrigger asChild>
+      <Button intent="secondary">Open Menu</Button>
+    </DropdownTrigger>
+    <DropdownContent>
+      <DropdownItem icon={IconUser}>Profile</DropdownItem>
+      <DropdownItem icon={IconSettings}>Settings</DropdownItem>
+      <DropdownSeparator />
+      <DropdownItem icon={IconLogout} intent="danger">
+        Logout
+      </DropdownItem>
+    </DropdownContent>
+  </Dropdown>`,...(w=(D=s.parameters)==null?void 0:D.docs)==null?void 0:w.source}}};var g,I,u;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`() => <Dropdown>
+    <DropdownTrigger asChild>
+      <Button intent="ghost" icon={IconDots} />
+    </DropdownTrigger>
+    <DropdownContent>
+      <DropdownItem icon={IconEdit}>Edit</DropdownItem>
+      <DropdownItem icon={IconTrash} intent="danger">
+        Delete
+      </DropdownItem>
+    </DropdownContent>
+  </Dropdown>`,...(u=(I=i.parameters)==null?void 0:I.docs)==null?void 0:u.source}}};var x,j,y;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`() => <Dropdown>
+    <DropdownTrigger asChild>
+      <Button intent="secondary">Account Menu</Button>
+    </DropdownTrigger>
+    <DropdownContent>
+      <DropdownLabel>Account</DropdownLabel>
+      <DropdownItem icon={IconUser}>Profile</DropdownItem>
+      <DropdownItem icon={IconSettings}>Settings</DropdownItem>
+      <DropdownSeparator />
+      <DropdownLabel>Actions</DropdownLabel>
+      <DropdownItem icon={IconLogout} intent="danger">
+        Logout
+      </DropdownItem>
+    </DropdownContent>
+  </Dropdown>`,...(y=(j=a.parameters)==null?void 0:j.docs)==null?void 0:y.source}}};var C,M,v;p.parameters={...p.parameters,docs:{...(C=p.parameters)==null?void 0:C.docs,source:{originalSource:`() => <div className="flex gap-4">
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">Default</Button>
+      </DropdownTrigger>
+      <DropdownContent>
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem>Another Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">With Disabled</Button>
+      </DropdownTrigger>
+      <DropdownContent>
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem disabled>Disabled Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+  </div>`,...(v=(M=p.parameters)==null?void 0:M.docs)==null?void 0:v.source}}};var S,f,T;c.parameters={...c.parameters,docs:{...(S=c.parameters)==null?void 0:S.docs,source:{originalSource:`() => <div className="flex gap-4">
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">Small</Button>
+      </DropdownTrigger>
+      <DropdownContent size="sm">
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem>Another Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">Medium</Button>
+      </DropdownTrigger>
+      <DropdownContent size="md">
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem>Another Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">Large</Button>
+      </DropdownTrigger>
+      <DropdownContent size="lg">
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem>Another Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+  </div>`,...(T=(f=c.parameters)==null?void 0:f.docs)==null?void 0:T.source}}};const gn=["Default","WithIcon","WithLabels","States","Sizes"];export{s as Default,c as Sizes,p as States,i as WithIcon,a as WithLabels,gn as __namedExportsOrder,wn as default};
---
+++ assets/Input.stories-AkXPtXf0.js
@@ -0,0 +1,31 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{I as h}from"./Input-MapL3597.js";import{c as e}from"./createReactComponent-DIAtsXst.js";import{I as n}from"./IconChevronDown-BTR_O2NM.js";import{I}from"./IconUser-Cl9KeqG0.js";import"./iframe-Bn-bUIPO.js";import"./preload-helper-Dp1pzeXC.js";import"./index-CRGxjvZy.js";import"./utils-BCsZDoRo.js";import"./bundle-mjs-DAWI7bSt.js";/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=[["path",{d:"M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z",key:"svg-0"}],["path",{d:"M16 3v4",key:"svg-1"}],["path",{d:"M8 3v4",key:"svg-2"}],["path",{d:"M4 11h16",key:"svg-3"}],["path",{d:"M11 15h1",key:"svg-4"}],["path",{d:"M12 15v3",key:"svg-5"}]],v=e("outline","calendar","Calendar",g);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=[["path",{d:"M10.585 10.587a2 2 0 0 0 2.829 2.828",key:"svg-0"}],["path",{d:"M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87",key:"svg-1"}],["path",{d:"M3 3l18 18",key:"svg-2"}]],k=e("outline","eye-off","EyeOff",m);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const M=[["path",{d:"M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0",key:"svg-0"}],["path",{d:"M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6",key:"svg-1"}]],t=e("outline","eye","Eye",M);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=[["path",{d:"M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z",key:"svg-0"}],["path",{d:"M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0",key:"svg-1"}],["path",{d:"M8 11v-4a4 4 0 1 1 8 0v4",key:"svg-2"}]],z=e("outline","lock","Lock",f);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=[["path",{d:"M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z",key:"svg-0"}],["path",{d:"M3 7l9 6l9 -6",key:"svg-1"}]],c=e("outline","mail","Mail",u);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const _=[["path",{d:"M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0",key:"svg-0"}],["path",{d:"M21 21l-6 -6",key:"svg-1"}]],s=e("outline","search","Search",_),i={"IconSearch (size 16)":s,"IconMail (size 16)":c,"IconUser (size 16)":I,"IconCalendar (size 16)":v,"IconLock (size 16)":z,"IconSearch (size 20)":a.jsx(s,{size:20}),"IconMail (size 20)":a.jsx(c,{size:20}),None:void 0},r={"IconEye (size 16)":t,"IconEyeOff (size 16)":k,"IconChevronDown (size 16)":n,"IconCalendar (size 16)":v,"IconEye (size 20)":a.jsx(t,{size:20}),"IconChevronDown (size 20)":a.jsx(n,{size:20}),None:void 0},w={title:"Components/Input",component:h,argTypes:{disabled:{control:{type:"boolean"}},invalid:{control:{type:"boolean"}},placeholder:{control:{type:"text"}},prefixIcon:{control:{type:"select"},options:Object.keys(i),mapping:i},trailingIcon:{control:{type:"select"},options:Object.keys(r),mapping:r},onTrailingIconClick:{action:"trailing icon clicked",table:{disable:!0}}}},x=y=>a.jsx(h,{...y}),o=x.bind({});o.args={placeholder:"Enter text...",ontrailingIconClick:()=>alert("Trailing icon clicked!"),disabled:!1,invalid:!1};var p,l,d;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:"args => <Input {...args} />",...(d=(l=o.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const L=["Default"];export{o as Default,L as __namedExportsOrder,w as default};
