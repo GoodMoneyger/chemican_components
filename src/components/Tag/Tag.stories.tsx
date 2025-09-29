123ad1c69b47b2bc55bb6c8302fd2a859b5a9b49
Kevin Schiffer <mail@kevinschiffer.com>
2025-09-29 13:53:56 +0900

    tag: Update styling to latest design and add color code prop

--- src/components/Tag/Tag.stories.tsx
+++ src/components/Tag/Tag.stories.tsx
@@ -6,10 +6,41 @@ import { ColorShapeTokens } from '../../tokens';
 import type { TagProps } from './Tag';
 import { Tag } from './Tag';
 
-export default {
+const meta: Meta<typeof Tag> = {
   title: 'Components/Tag',
   component: Tag,
-} as Meta;
+  argTypes: {
+    accentColor: {
+      control: 'select',
+      options: ColorShapeTokens,
+    },
+    colorCode: {
+      control: 'select',
+      options: [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+      ],
+    },
+    size: {
+      control: 'select',
+      options: ['sm', 'md'],
+    },
+    onRemove: {
+      control: 'boolean',
+      defaultValue: false,
+      description: 'Enable or disable the onRemove functionality',
+      table: {
+        type: { summary: 'function' },
+        defaultValue: { summary: 'undefined' },
+      },
+      mapping: {
+        true: () => alert('Tag removed'),
+        false: undefined,
+      },
+    },
+  },
+};
+
+export default meta;
 
 const TagTemplate: StoryFn<TagProps> = (args) => <Tag {...args} />;
 
@@ -18,30 +49,6 @@ DefaultTag.args = {
   children: 'タグ',
   onRemove: undefined,
   accentColor: ColorShapeTokens.AccentBlueSoft,
-};
-
-DefaultTag.argTypes = {
-  accentColor: {
-    control: 'select',
-    options: ColorShapeTokens,
-  },
-  size: {
-    control: 'select',
-    options: ['sm', 'md'],
-  },
-  onRemove: {
-    control: 'boolean',
-    defaultValue: false,
-    description: 'Enable or disable the onRemove functionality',
-    table: {
-      type: { summary: 'function' },
-      defaultValue: { summary: 'undefined' },
-    },
-    mapping: {
-      true: () => alert('Tag removed'),
-      false: undefined,
-    },
-  },
 };
 
 export const MediumSize = TagTemplate.bind({});
@@ -57,4 +64,31 @@ WithRemoveButton.args = {
   size: 'sm',
   accentColor: ColorShapeTokens.AccentRedSoft,
   onRemove: () => alert('Tag removed!'),
+};
+
+export const WithColorCode = TagTemplate.bind({});
+WithColorCode.args = {
+  children: 'カラーコード1',
+  colorCode: 1,
+};
+
+export const ColorCodeShowcase = () => (
+  <div className="gap-2 flex flex-wrap">
+    {Array.from({ length: 19 }, (_, i) => i).map((colorCode) => (
+      <Tag
+        key={colorCode}
+        // prettier-ignore
+        colorCode={colorCode as 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18}
+      >
+        カラー {colorCode}
+      </Tag>
+    ))}
+  </div>
+);
+
+export const ConflictingProps = TagTemplate.bind({});
+ConflictingProps.args = {
+  children: '競合プロパティ (コンソールを確認)',
+  colorCode: 5,
+  accentColor: ColorShapeTokens.AccentRedSoft,
 };
