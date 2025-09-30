ba9fd530a6cf51042c33854f47680acb1cb2364c
ThePrettyN <demiris.costa1229@gmail.com>
2025-09-30 09:07:41 +0900

    refactor(ui): use Tailwind utilities (spacing/state classes), remove unused variants, simplify logic, fix double border and so on

--- src/components/SearchBar/SearchBar.stories.tsx
+++ src/components/SearchBar/SearchBar.stories.tsx
@@ -10,15 +10,27 @@ const meta: Meta<typeof SearchBar> = {
       control: { type: 'select' },
       options: ['sm', 'md', 'lg'],
     },
-    disabled: {
-      control: { type: 'boolean' },
-    },
     placeholder: {
       control: { type: 'text' },
     },
     state: {
       control: { type: 'radio' },
       options: ['default', 'hover', 'focus', 'filled', 'disabled'],
+    },
+    value: {
+      table: { disable: true }, // Hide from controls
+    },
+    onChange: {
+      table: { disable: true }, // Hide from controls
+    },
+    onSearch: {
+      table: { disable: true }, // Hide from controls
+    },
+    searchButtonText: {
+      table: { disable: true }, // Hide from controls
+    },
+    disabled: {
+      table: { disable: true }, // Hide from controls
     },
   },
 };
@@ -32,7 +44,6 @@ const Template: StoryFn<typeof SearchBar> = (args) => {
       {...args}
       value={value}
       onChange={(e) => setValue(e.target.value)}
-      onClear={() => setValue('')}
       onSearch={() => alert('Search: ' + value)}
       state={args.state} // <-- pass state prop
     />
@@ -44,7 +55,6 @@ Default.args = {
   placeholder: 'SDS ID/SDSファイル名/製品名/化学物質名/CAS番号/法令名/細則名',
   supportText:
     'Enterキーを押して、キーワードを確定してください。キーワードは複数入力することができます。',
-  disabled: false,
   size: 'md',
   state: 'default', // <-- set initial state
 };
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,30 +1,28 @@
 import React, { useEffect } from 'react';
 import type { VariantProps } from 'class-variance-authority';
 import { cva } from 'class-variance-authority';
-import classNames from 'classnames';
-import { twMerge } from 'tailwind-merge';
 import { IconSearch, IconX } from '@tabler/icons-react';
 
+import { cn } from '../../lib/utils';
+
 const searchBarWrapperVariants = cva(
-  `rounded-sm bg-white min-h-8 flex w-auto max-w-full min-w-[448px] items-center
-  justify-between border`,
+  `rounded-sm bg-white min-h-8 focus-within:border-interactive-hover
+  hover:border-interactive-hover flex w-auto max-w-full items-center
+  justify-between border focus-within:shadow-[0_0_0_4px_#C9E8E5]`,
   {
     variants: {
       size: {
-        sm: 'h-8 text-sm w-auto max-w-full min-w-[400px]',
-        md: 'h-10 text-base w-auto max-w-full min-w-[448px]',
-        lg: 'h-12 text-lg w-auto max-w-full min-w-[500px]',
+        sm: 'h-8 text-sm',
+        md: 'h-10 text-base',
+        lg: 'h-12 text-lg',
       },
       state: {
-        default: 'border-[var(--border-color-interactive-default)]',
-        hover: 'hover:border-[var(--border-color-interactive-hover)]',
-        focus: `border-[var(--border-color-interactive-hover)]
-        shadow-[0_0_0_4px_#C9E8E5]`,
-        filled: 'border-[var(--border-color-interactive-default)]',
-        disabled: `cursor-not-allowed
-        border-[var(--border-color-interactive-default)]
-        bg-[var(--background-color-input-disabled)]
-        text-[var(--text-color-body-disabled)]`,
+        default: 'border-interactive-default',
+        filled: 'border-interactive-default',
+        hover: '!border-interactive-hover',
+        focus: '!border-interactive-hover !shadow-[0_0_0_4px_#C9E8E5]',
+        disabled: `!border-interactive-default bg-input-disabled
+        text-body-disabled pointer-events-none cursor-not-allowed`,
       },
     },
     defaultVariants: {
@@ -35,22 +33,14 @@ const searchBarWrapperVariants = cva(
 );
 
 const inputWrapperVariants = cva(
-  `min-w-0 flex flex-1 items-center gap-[var(--spacing-xxs)]
-  px-[var(--spacing-sm)]`, //overflow-x-auto
+  'gap-xxs px-sm flex flex-1 items-center', //overflow-x-auto
   {
     variants: {
-      size: {
-        sm: '',
-        md: '',
-        lg: '',
-      },
       disabled: {
-        true: 'bg-[var(--background-color-input-disabled)]',
-        false: '',
+        true: 'bg-input-disabled',
       },
     },
     defaultVariants: {
-      size: 'md',
       disabled: false,
     },
   }
@@ -58,139 +48,78 @@ const inputWrapperVariants = cva(
 
 // New inputGroupVariants for search icon + input field //
 const inputGroupVariants = cva(
-  `min-w-0 rounded-l-sm gap-4] flex flex-1 flex-row flex-nowrap items-center
+  `rounded-l-sm gap-4 flex h-full flex-1 flex-row flex-nowrap items-center
   justify-between`,
   {
     variants: {
-      size: {
-        sm: '',
-        md: '',
-        lg: '',
-      },
       disabled: {
-        true: 'bg-[var(--background-color-input-disabled)]',
-        false: '',
-      },
-      state: {
-        default: '',
-        hover: '',
-        focus: '',
-        filled: '',
-        disabled: '',
+        true: 'bg-input-disabled',
       },
     },
     defaultVariants: {
-      size: 'md',
       disabled: false,
     },
   }
 );
 
-const searchIconVariants = cva(
-  'flex items-center text-[var(--color-shape-primary)]',
-  {
-    variants: {
-      size: {
-        sm: 'h-[20px] w-[20px]',
-        md: 'h-[20px] w-[20px]',
-        lg: 'h-[20px] w-[20px]',
-      },
-      state: {
-        default: 'text-[var(--color-shape-primary)]',
-        hover: 'text-[var(--color-shape-primary)]',
-        focus: 'text-[var(--color-shape-primary)]',
-        filled: 'text-[var(--color-shape-primary)]',
-        disabled: 'text-[var(--color-shape-interactive-disabled)]',
-      },
+const searchIconVariants = cva('text-shape-primary h-5 w-5 flex items-center', {
+  variants: {
+    state: {
+      disabled: 'text-shape-interactive-disabled',
     },
-    defaultVariants: {
-      size: 'md',
-    },
-  }
-);
+  },
+});
 
 const inputVariants = cva(
-  `min-w-[120px] flex-1 bg-transparent leading-[100%] tracking-[0%]
-  text-[var(--text-size-md)] outline-none`,
+  `text-md flex-1 bg-transparent leading-[100%] tracking-[0%] outline-none
+  focus:placeholder-transparent`,
   {
     variants: {
-      size: {
-        sm: '',
-        md: '',
-        lg: '',
-      },
       state: {
-        default: 'text-[var(--text-color-body-disabled)]',
-        hover: '',
-        focus: '',
-        filled: '',
-        disabled: `cursor-not-allowed
-        bg-[var(--background-color-input-disabled)]
-        text-[var(--text-color-body-disabled)]`, // background and text for disabled
+        default: 'text-body-disabled',
+        disabled: 'bg-input-disabled text-body-disabled cursor-not-allowed', // background and text for disabled
       },
       disabled: {
-        true: `cursor-not-allowed bg-[var(--background-color-input-disabled)]
-        text-[var(--text-color-body-disabled)]`, // background and text for disabled
+        true: 'bg-input-disabled text-body-disabled cursor-not-allowed', // background and text for disabled
         false: 'text-body-primary',
       },
     },
     defaultVariants: {
-      size: 'md',
       disabled: false,
     },
   }
 );
 
 const buttonVariants = cva(
-  `gap-4 rounded-r-sm flex h-[40px] min-w-[60px] flex-row items-center
-  justify-center border bg-[var(--color-shape-accent-gray-pale)]
-  px-[var(--spacing-sm)] py-[var(--spacing-xs)] text-center
-  text-[var(--text-size-md)]
-  hover:border-[var(--border-color-interactive-hover)]
-  hover:text-[var(--text-color-interactive-primary-hover)]`,
+  `gap-4 rounded-r-sm bg-shape-accent-gray-pale px-sm py-xs text-shape-primary
+  group-hover:border-t-interactive-hover group-hover:border-b-interactive-hover
+  group-hover:border-l-interactive-default
+  group-hover:text-interactive-primary-hover
+  group-focus-within:border-t-interactive-hover
+  group-focus-within:border-b-interactive-hover
+  group-focus-within:border-l-interactive-default
+  group-focus-within:text-shape-primary flex cursor-pointer flex-row
+  items-center justify-center border-t border-b border-l text-center
+  hover:cursor-pointer`,
   {
     variants: {
       size: {
-        sm: 'h-8 text-[var(--text-size-sm)]',
-        md: 'h-10 text-[var(--text-size-md)]',
-        lg: 'h-12 text-[var(--text-size-lg)]',
-      },
-      disabled: {
-        true: 'bg-gray-100 text-gray-400 cursor-not-allowed',
-        false: '',
+        sm: 'h-8 text-sm',
+        md: 'h-10 text-md',
+        lg: 'h-12 text-lg',
       },
       state: {
-        default: `border-[var(--border-color-interactive-default)]
-        text-[var(--color-shape-primary)]`,
-        hover: `border-t-[var(--border-color-interactive-hover)]
-        border-r-[var(--border-color-interactive-hover)]
-        border-b-[var(--border-color-interactive-hover)]
-        border-l-[var(--border-color-interactive-default)]
-        text-[var(--text-color-interactive-primary-hover)]`,
-        focus: `border-l-[var(--border-color-interactive-default)]
-        text-[var(--color-shape-primary)]`,
-        filled: `border-[var(--border-color-interactive-default)]
-        text-[var(--color-shape-primary)]`,
-        disabled: `cursor-not-allowed
-        border-l-[var(--border-color-interactive-disabled)]
-        bg-[var(--color-shape-accent-gray-pale)]
-        text-[--text-color-body-disabled]`,
-      },
-    },
-    defaultVariants: {
-      size: 'md',
-      disabled: false,
-    },
-  }
-);
-const supportTextVariants = cva(
-  'gap-2 text-sm flex flex-row text-[var(--text-color-body-secondary)]',
-  {
-    variants: {
-      size: {
-        sm: 'w-auto max-w-full min-w-[400px]',
-        md: 'w-auto max-w-full min-w-[448px]',
-        lg: 'w-auto max-w-full min-w-[500px]',
+        default: `border-t-interactive-default border-b-interactive-default
+        border-l-interactive-default`,
+        filled: `border-t-interactive-default border-b-interactive-default
+        border-l-interactive-default`,
+        hover: `!border-t-interactive-hover !border-b-interactive-hover
+        !border-l-interactive-default !text-interactive-primary-hover`,
+        focus: `!border-t-interactive-hover !border-b-interactive-hover
+        !border-l-interactive-default !text-shape-primary`,
+        disabled: `!border-t-interactive-default !border-b-interactive-default
+        !border-l-interactive-default !bg-shape-accent-gray-pale
+        !text-body-disabled !cursor-not-allowed`,
       },
     },
     defaultVariants: {
@@ -198,38 +127,28 @@ const supportTextVariants = cva(
     },
   }
 );
+const supportTextVariants = cva('gap-2 text-sm text-body-secondary flex-row');
 const chipVariants = cva(
-  `gap-1 flex items-center rounded-full bg-[var(--color-shape-accent-gray-pale)]
-  px-[var(--spacing-xs)] py-[var(--spacing-xxs)]
-  text-[var(--text-color-accent-gray-strong)] text-[var(--text-md)]`,
+  `gap-1 bg-shape-accent-gray-pale px-xs py-xxs text-md text-accent-gray-strong
+  flex items-center rounded-full`,
   {
     variants: {
       size: {
-        sm: 'h-[20px]',
-        md: 'h-[22px]',
-        lg: 'h-[22px]',
-      },
-      radius: {
-        max: '', // rounded-full already applies max radius
+        sm: 'h-5',
+        md: 'h-6',
+        lg: 'h-6',
       },
     },
     defaultVariants: {
       size: 'md',
-      radius: 'max',
     },
   }
 );
 const chipRemoveButtonVariants = cva(
-  `bg-white w-3 h-3 gap-2.5 flex items-center justify-center rounded-full
-  p-[var(--spacing-xxs)] text-[var(--color-shape-primary)]`
+  'h-3 w-3 text-shape-primary flex items-center justify-center rounded-full'
 );
 const chipContainerVariants = cva(
-  `gap-2 flex max-h-[48px] min-h-[32px] min-h-full flex-1 flex-row flex-nowrap
-  items-center`
-);
-const chipIconX = cva(
-  `p-1 gap-2.5 flex h-[12px] w-[12px] items-center justify-center rounded-full
-  bg-[#FFFFFF]` // p-1 = 4px, gap-2.5 = 10px
+  'gap-2 flex min-h-full flex-1 flex-row flex-nowrap items-center'
 );
 export interface SearchBarProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'>,
@@ -240,8 +159,9 @@ export interface SearchBarProps
   placeholder?: string;
   disabled?: boolean;
   size?: 'sm' | 'md' | 'lg';
-  state?: 'default' | 'hover' | 'focus' | 'filled' | 'disabled';
+  state?: 'default' | 'filled' | 'disabled' | 'hover' | 'focus';
   supportText?: string;
+  searchButtonText?: string;
 }
 
 const iconSizeMap = {
@@ -259,34 +179,24 @@ export const SearchBar: React.FC<SearchBarProps> = ({
   placeholder,
   disabled: disabledProp,
   supportText,
+  searchButtonText = '検索',
   ...props
 }) => {
   const disabled = disabledProp || state === 'disabled';
 
-  const [isFocused, setIsFocused] = React.useState(false);
-  const [isHovered, setIsHovered] = React.useState(false); //
   const [keywords, setKeywords] = React.useState<string[]>([]);
-  const [firstFocus, setFirstFocus] = React.useState(false);
 
-  // Automatically set keywords to ['ファイル'] if state is 'filled' from Storybook
-  useEffect(() => {
-    if (state === 'filled') {
-      setKeywords(['ファイル']);
-    }
-  }, [state]);
+  let resolvedState: 'default' | 'filled' | 'disabled' | 'hover' | 'focus';
 
-  const resolvedState =
-    typeof state === 'string'
-      ? state
-      : disabled
-        ? 'disabled'
-        : isFocused
-          ? 'focus'
-          : isHovered
-            ? 'hover'
-            : keywords.length > 0
-              ? 'filled'
-              : 'default';
+  if (typeof state === 'string') {
+    resolvedState = state;
+  } else if (disabled) {
+    resolvedState = 'disabled';
+  } else if (keywords.length > 0) {
+    resolvedState = 'filled';
+  } else {
+    resolvedState = 'default';
+  }
   const prevResolvedState = React.useRef(resolvedState);
 
   useEffect(() => {
@@ -296,27 +206,12 @@ export const SearchBar: React.FC<SearchBarProps> = ({
     }
     prevResolvedState.current = resolvedState;
   }, [resolvedState]);
+
   // Handle input change
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (disabled) return; // Prevent changes if disabled
     if (onChange) onChange(e);
   };
-
-  useEffect(() => {
-    // If transitioning from 'focus' to another state, clear input value
-    if (
-      prevResolvedState.current === 'focus' &&
-      resolvedState !== 'focus' &&
-      value
-    ) {
-      if (onChange) {
-        onChange({
-          target: { value: '' },
-        } as React.ChangeEvent<HTMLInputElement>);
-      }
-    }
-    prevResolvedState.current = resolvedState;
-  }, [resolvedState, value, onChange]);
 
   // Handle Enter key to confirm keyword
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -347,65 +242,36 @@ export const SearchBar: React.FC<SearchBarProps> = ({
     if (disabled) return; // Prevent changes if disabled
     setKeywords(keywords.filter((_, i) => i !== idx));
   };
-  // Prevent focus when disabled
-  const handleInputFocus = () => {
-    if (!disabled) {
-      setIsFocused(true);
-      if (!firstFocus && !value && resolvedState === 'focus') {
-        if (onChange) {
-          // Set value to 'ファイル' only on first focus
-          const event = {
-            target: { value: 'ファイル' },
-          } as React.ChangeEvent<HTMLInputElement>;
-          onChange(event);
-        }
-        setFirstFocus(true);
-      }
-    }
-  };
-  const handleInputBlur = () => {
-    if (!disabled) setIsFocused(false);
-  };
+
   return (
     <div
-      className="gap-1 flex flex-col"
-      style={disabled ? { pointerEvents: 'none' } : {}}
+      className={cn(
+        'gap-1 group flex flex-col',
+        disabled ? 'pointer-events-none' : ''
+      )}
       aria-disabled={disabled ? 'true' : undefined}
     >
       <div
-        onMouseEnter={() => !disabled && setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        className={twMerge(
-          classNames(
-            searchBarWrapperVariants({
-              size,
-              state: resolvedState,
-            })
-          )
+        className={cn(
+          searchBarWrapperVariants({
+            size,
+            state: resolvedState,
+          })
         )}
       >
         {/* Chips + Input area */}
-        <div
-          className={twMerge(
-            classNames(inputWrapperVariants({ size, disabled }))
-          )}
-        >
+        <div className={cn(inputWrapperVariants({ disabled }))}>
           <div
-            className={twMerge(
-              classNames(
-                chipContainerVariants(),
-                inputGroupVariants({ size, disabled, state: resolvedState })
-              )
+            className={cn(
+              chipContainerVariants(),
+              inputGroupVariants({ disabled })
             )}
           >
             <span
-              className={twMerge(
-                classNames(
-                  searchIconVariants({
-                    size,
-                    state: disabled ? 'disabled' : 'default',
-                  })
-                )
+              className={cn(
+                searchIconVariants({
+                  ...(disabled && { state: 'disabled' }),
+                })
               )}
             >
               <IconSearch size={iconSizeMap[size]} />
@@ -421,53 +287,29 @@ export const SearchBar: React.FC<SearchBarProps> = ({
                   tabIndex={-1}
                   aria-label="Remove keyword"
                   disabled={disabled} // Disable chip remove button
+                  style={{ backgroundColor: '#ffffff' }}
                 >
-                  <div className={chipIconX()}>
-                    {/* <IconX size={32} /> */}
-                    <svg
-                      width="18"
-                      height="18"
-                      viewBox="0 0 6 6"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M5.2168 0.784518C5.37391 0.941625 5.37391 1.19635 5.2168 1.35345L1.35474 5.21552C1.19763 5.37263 0.942907 5.37263 0.785799 5.21552C0.628692 5.05842 0.628692 4.80369 0.785799 4.64659L4.64787 0.784518C4.80498 0.62741 5.0597 0.62741 5.2168 0.784518Z"
-                        fill="#143028"
-                      />
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0.785799 0.784518C0.942907 0.62741 1.19763 0.62741 1.35474 0.784518L5.2168 4.64659C5.37391 4.80369 5.37391 5.05842 5.2168 5.21552C5.0597 5.37263 4.80498 5.37263 4.64787 5.21552L0.785799 1.35345C0.628692 1.19635 0.628692 0.941625 0.785799 0.784518Z"
-                        fill="#143028"
-                      />
-                    </svg>
-                  </div>
+                  <IconX size={8} />
                 </button>
               </span>
             ))}
             <input
-              className={twMerge(
-                classNames(
-                  inputVariants({ size, disabled, state: resolvedState })
-                )
+              className={cn(
+                inputVariants({
+                  disabled,
+                  state: resolvedState === 'disabled' ? 'disabled' : 'default',
+                })
               )}
               type="text"
               value={value}
               onChange={handleInputChange}
               onKeyDown={handleKeyDown}
               placeholder={
-                resolvedState === 'filled' || resolvedState === 'focus'
+                resolvedState === 'filled' || keywords.length > 0
                   ? ''
-                  : !isFocused && !value
-                    ? placeholder
-                    : ''
+                  : placeholder
               }
               disabled={disabled}
-              onFocus={handleInputFocus}
-              onBlur={handleInputBlur}
               {...props}
             />
           </div>
@@ -475,7 +317,7 @@ export const SearchBar: React.FC<SearchBarProps> = ({
             <button
               type="button"
               aria-label="Clear"
-              className="text-[var(--color-shape-primary)]"
+              className="text-shape-primary"
               onClick={() => {
                 if (onChange) {
                   onChange({
@@ -493,26 +335,30 @@ export const SearchBar: React.FC<SearchBarProps> = ({
         <div className="flex h-full items-center">
           <button
             type="button"
-            className={twMerge(
-              classNames(
-                buttonVariants({
-                  size,
-                  disabled,
-                  state: resolvedState,
-                })
-              )
+            className={cn(
+              buttonVariants({
+                size,
+                state: resolvedState,
+              })
             )}
             onClick={onSearch}
             disabled={disabled}
           >
-            検索
+            {searchButtonText}
           </button>
         </div>
       </div>
-      {(isFocused || resolvedState === 'focus' || resolvedState === 'filled') &&
-        supportText && (
-          <div className={supportTextVariants({ size })}>{supportText}</div>
-        )}
+      {supportText && (
+        <div
+          className={cn(
+            supportTextVariants(),
+            'hidden group-focus-within:flex',
+            (resolvedState === 'filled' || resolvedState === 'focus') && 'flex'
+          )}
+        >
+          {supportText}
+        </div>
+      )}
     </div>
   );
 };
