79257a873e844568b7942b6a229c5e7b4c436d40
Kevin Schiffer <mail@kevinschiffer.com>
2025-10-20 17:54:42 +0900

    formfield: Fix font size to small

--- src/components/FormField/FormField.tsx
+++ src/components/FormField/FormField.tsx
@@ -27,7 +27,7 @@ export const FormField: React.FC<FormFieldProps> = ({
         <label
           htmlFor={name}
           className={cn(
-            `text-body-secondary gap-1 pb-2 text-xs font-normal flex
+            `text-body-secondary gap-1 pb-2 text-sm font-normal flex
             items-center leading-none`,
             className
           )}
@@ -39,14 +39,14 @@ export const FormField: React.FC<FormFieldProps> = ({
       {children}
       {error && (
         <div className="pt-1">
-          <p className="text-body-alert text-xs font-normal leading-[1.5]">
+          <p className="text-body-alert text-sm font-normal leading-[1.5]">
             {error}
           </p>
         </div>
       )}
       {description && (
         <div className="pt-1">
-          <p className="text-body-secondary text-xs font-normal leading-[1.5]">
+          <p className="text-body-secondary text-sm font-normal leading-[1.5]">
             {description}
           </p>
         </div>
