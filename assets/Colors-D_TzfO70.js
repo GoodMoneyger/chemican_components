6d8537e89ddefa69589df66b695b1fb451afd0a0
kschiffer <mail@kevinschiffer.com>
2026-02-03 01:03:20 +0000

    Deploying to gh-pages from @ GoodMoneyger/chemican_components@0ef981279c8d3ac44a31617fe40536d2537ffe22 🚀

---
+++ assets/Colors-D_TzfO70.js
@@ -0,0 +1,7 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-DvnRHqLL.js";import{M as i}from"./blocks-DFR6-J7l.js";import{C as t}from"./Colors-BBqM1n0P.js";import"./iframe-CQGyqvgL.js";import"./preload-helper-Dp1pzeXC.js";import"./index-CrtKEqZH.js";import"./index-Bx6PrWJx.js";import"./utils-CX0QYg5F.js";import"./tokens-ClON5slf.js";function r(n){const e={h1:"h1",h2:"h2",p:"p",strong:"strong",...s(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{title:"Design System/Tokens/Colors",component:t}),`
+`,o.jsx(e.h1,{id:"color-tokens",children:"Color Tokens"}),`
+`,o.jsx(e.p,{children:"This document provides an overview of the color tokens used in our design system. The color tokens are categorized into background, text, border, and shape colors."}),`
+`,o.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(e.p,{children:"Please note that utility classes are available only for the respective property. For example, tokens for border color cannot be used for a background."}),`
+`,o.jsxs(e.p,{children:["➡ If you believe a color is missing, please notify the design team and ",o.jsx(e.strong,{children:"do not use unfitting tokens"}),"."]}),`
+`,o.jsx(t,{})]})}function g(n={}){const{wrapper:e}={...s(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(r,{...n})}):r(n)}export{g as default};
--- assets/index-DJMbtA3D.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-DyWolvg4.js";import{r as o}from"./index-CHfvmqer.js";var t=o();const m=r(t);export{m as F,t as r};
