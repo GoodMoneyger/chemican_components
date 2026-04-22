46ef48d523fe72225b2b782c77e0dd2767cd5a33
AmbegodaPraneeth <98864918+AmbegodaPraneeth@users.noreply.github.com>
2026-04-22 04:24:08 +0000

    Deploying to gh-pages from @ GoodMoneyger/chemican_components@fcc5fc1ef577e3a0af41ba8036239d5cf79c32c3 🚀

---
+++ assets/DropdownMenu.stories-Ccu7QqO1.js
@@ -0,0 +1,92 @@
+import{j as n}from"./iframe-BAwDJuLJ.js";import{B as r}from"./Button-BclXecnI.js";import{D as e,a as t,b as d,c as o,d as m,e as b}from"./DropdownMenu-C7yHDZKE.js";import{c as v}from"./createReactComponent-OmAIA7bo.js";import{I as z}from"./IconEdit-zCX15usi.js";import{I as N}from"./IconTrash-MbOlV18b.js";import{I as B}from"./IconUser-BX0KEOhX.js";import{I as L}from"./IconSettings-C96ay-Jb.js";import{I as A}from"./IconLogout-CLALD5VX.js";import"./preload-helper-Dp1pzeXC.js";import"./ProgressIndicator-BWfGRNIJ.js";import"./index-DC0v3VNi.js";import"./index-B0VATQDm.js";import"./index-CAjpj3cn.js";import"./index-CAS30g0P.js";/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const W=[["path",{d:"M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-0"}],["path",{d:"M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-1"}],["path",{d:"M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-2"}]],E=v("outline","dots","Dots",W),$={title:"Components/DropdownMenu",component:e,parameters:{layout:"centered"},argTypes:{open:{control:{type:"boolean"},description:"Controls whether the dropdown is open"},modal:{control:{type:"boolean"},description:"Whether the dropdown is modal"}}},P=_=>n.jsxs(e,{..._,children:[n.jsx(t,{asChild:!0,children:n.jsx(r,{intent:"secondary",children:"Open Menu"})}),n.jsxs(d,{children:[n.jsx(o,{icon:B,children:"Profile"}),n.jsx(o,{icon:L,children:"Settings"}),n.jsx(b,{}),n.jsx(o,{icon:A,intent:"danger",children:"Logout"})]})]}),s=P.bind({});s.args={};const i=()=>n.jsxs(e,{children:[n.jsx(t,{asChild:!0,children:n.jsx(r,{intent:"text",icon:E})}),n.jsxs(d,{children:[n.jsx(o,{icon:z,children:"Edit"}),n.jsx(o,{icon:N,intent:"danger",children:"Delete"})]})]}),a=()=>n.jsxs(e,{children:[n.jsx(t,{asChild:!0,children:n.jsx(r,{intent:"secondary",children:"Account Menu"})}),n.jsxs(d,{children:[n.jsx(m,{children:"Account"}),n.jsx(o,{icon:B,children:"Profile"}),n.jsx(o,{icon:L,children:"Settings"}),n.jsx(b,{}),n.jsx(m,{children:"Actions"}),n.jsx(o,{icon:A,intent:"danger",children:"Logout"})]})]}),c=()=>n.jsxs("div",{className:"gap-4 flex",children:[n.jsxs(e,{children:[n.jsx(t,{asChild:!0,children:n.jsx(r,{intent:"secondary",children:"Default"})}),n.jsxs(d,{children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{children:"Another Item"})]})]}),n.jsxs(e,{children:[n.jsx(t,{asChild:!0,children:n.jsx(r,{intent:"secondary",children:"With Disabled"})}),n.jsxs(d,{children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{disabled:!0,children:"Disabled Item"})]})]})]}),p=()=>n.jsxs("div",{className:"gap-4 flex",children:[n.jsxs(e,{children:[n.jsx(t,{asChild:!0,children:n.jsx(r,{intent:"secondary",children:"Small"})}),n.jsxs(d,{size:"sm",children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{children:"Another Item"})]})]}),n.jsxs(e,{children:[n.jsx(t,{asChild:!0,children:n.jsx(r,{intent:"secondary",children:"Medium"})}),n.jsxs(d,{size:"md",children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{children:"Another Item"})]})]}),n.jsxs(e,{children:[n.jsx(t,{asChild:!0,children:n.jsx(r,{intent:"secondary",children:"Large"})}),n.jsxs(d,{size:"lg",children:[n.jsx(o,{children:"Menu Item"}),n.jsx(o,{children:"Another Item"})]})]})]});i.__docgenInfo={description:"",methods:[],displayName:"WithIcon"};a.__docgenInfo={description:"",methods:[],displayName:"WithLabels"};c.__docgenInfo={description:"",methods:[],displayName:"States"};p.__docgenInfo={description:"",methods:[],displayName:"Sizes"};var l,D,h;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`args => <Dropdown {...args}>
+    <DropdownTrigger asChild>
+      <Button intent="secondary">Open Menu</Button>
+    </DropdownTrigger>
+    <DropdownContent>
+      <DropdownItem icon={IconUser}>Profile</DropdownItem>
+      <DropdownItem icon={IconSettings}>Settings</DropdownItem>
+      <DropdownSeparator />
+      <DropdownItem icon={IconLogout} intent="danger">
+        Logout
+      </DropdownItem>
+    </DropdownContent>
+  </Dropdown>`,...(h=(D=s.parameters)==null?void 0:D.docs)==null?void 0:h.source}}};var w,I,g;i.parameters={...i.parameters,docs:{...(w=i.parameters)==null?void 0:w.docs,source:{originalSource:`() => <Dropdown>
+    <DropdownTrigger asChild>
+      <Button intent="text" icon={IconDots} />
+    </DropdownTrigger>
+    <DropdownContent>
+      <DropdownItem icon={IconEdit}>Edit</DropdownItem>
+      <DropdownItem icon={IconTrash} intent="danger">
+        Delete
+      </DropdownItem>
+    </DropdownContent>
+  </Dropdown>`,...(g=(I=i.parameters)==null?void 0:I.docs)==null?void 0:g.source}}};var u,x,j;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`() => <Dropdown>
+    <DropdownTrigger asChild>
+      <Button intent="secondary">Account Menu</Button>
+    </DropdownTrigger>
+    <DropdownContent>
+      <DropdownLabel>Account</DropdownLabel>
+      <DropdownItem icon={IconUser}>Profile</DropdownItem>
+      <DropdownItem icon={IconSettings}>Settings</DropdownItem>
+      <DropdownSeparator />
+      <DropdownLabel>Actions</DropdownLabel>
+      <DropdownItem icon={IconLogout} intent="danger">
+        Logout
+      </DropdownItem>
+    </DropdownContent>
+  </Dropdown>`,...(j=(x=a.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};var C,f,y;c.parameters={...c.parameters,docs:{...(C=c.parameters)==null?void 0:C.docs,source:{originalSource:`() => <div className="gap-4 flex">
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">Default</Button>
+      </DropdownTrigger>
+      <DropdownContent>
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem>Another Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">With Disabled</Button>
+      </DropdownTrigger>
+      <DropdownContent>
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem disabled>Disabled Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+  </div>`,...(y=(f=c.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};var S,T,M;p.parameters={...p.parameters,docs:{...(S=p.parameters)==null?void 0:S.docs,source:{originalSource:`() => <div className="gap-4 flex">
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">Small</Button>
+      </DropdownTrigger>
+      <DropdownContent size="sm">
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem>Another Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">Medium</Button>
+      </DropdownTrigger>
+      <DropdownContent size="md">
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem>Another Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+
+    <Dropdown>
+      <DropdownTrigger asChild>
+        <Button intent="secondary">Large</Button>
+      </DropdownTrigger>
+      <DropdownContent size="lg">
+        <DropdownItem>Menu Item</DropdownItem>
+        <DropdownItem>Another Item</DropdownItem>
+      </DropdownContent>
+    </Dropdown>
+  </div>`,...(M=(T=p.parameters)==null?void 0:T.docs)==null?void 0:M.source}}};const nn=["Default","WithIcon","WithLabels","States","Sizes"];export{s as Default,p as Sizes,c as States,i as WithIcon,a as WithLabels,nn as __namedExportsOrder,$ as default};
--- assets/index-Cpu0QuHb.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./iframe-D_nQz3SH.js";var e=t.createContext(void 0);function i(r){const o=t.useContext(e);return r||o||"ltr"}export{i as u};
