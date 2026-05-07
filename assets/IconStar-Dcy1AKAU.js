00e72facf2cf8ec4fe69407b8c0bb5af0545c9a5
kschiffer <mail@kevinschiffer.com>
2026-05-07 02:26:27 +0000

    Deploying to gh-pages from @ GoodMoneyger/chemican_components@8ac671bcf08608da336441496493e2738fa5ea69 🚀

---
+++ assets/IconStar-Dcy1AKAU.js
@@ -0,0 +1,11 @@
+import{c as t}from"./createReactComponent-ChShNWeo.js";/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572",key:"svg-0"}]],l=t("outline","heart","Heart",o);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z",key:"svg-0"}]],n=t("outline","star","Star",a);export{n as I,l as a};
